8df4be68aa7785e67c63ce87f93422ae27e9fbf1
Ehab Elshahat <61202200+Ehab-Elshahat@users.noreply.github.com>
2023-02-20 22:06:20 +0200

    new Skills page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       animate={{ width: "100%" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.3 } }}
     >
-      <div className="container-fluid">
+      <div className="container">
         <nav>
           <div className="phone">
             <span>Phone: </span>
@@ -21,24 +21,39 @@ const Home = () => {
           </div>
         </nav>
         <header>
-          <div className="header-img 	d-none d-md-block">
-            <img src={headerImg} alt="Header Img" />
-          </div>
-          <div className="header-desc mt-md-5 ">
-            <h3>Hello, My name is</h3>
-            <h1>Ehab Elshahat</h1>
-            <h2>Front-end Developer</h2>
-            <p>
-              I program modern and interactive user interfaces that respond to
-              all screens
-            </p>
-            <a
-              className="download"
-              href="../../assets/img/1-23 CV.pdf"
-              download
-            >
-              Download CV
-            </a>
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-xs-12">
+              <div className="header-img 	d-none d-lg-block">
+                <img src={headerImg} alt="Header Img" />
+              </div>
+            </div>
+            <div className="col-lg-6 col-xs-12">
+              <div className="header-desc mt-md-5 ">
+                <h3>Hello, My name is</h3>
+                <h1>Ehab Elshahat</h1>
+                <h2>Front-end Developer</h2>
+                <p>
+                  Over the past several years, I have dedicated myself to
+                  learning various programming languages, including HTML, CSS,
+                  and JavaScript. I have also gained experience with various
+                  front-end frameworks such as React and Vue My passion for
+                  front-end development is driven by my love for building
+                  beautiful, user-friendly interfaces. I have always been
+                  fascinated by the way websites and web applications work, and
+                  I am constantly striving to improve my skills and knowledge in
+                  this field. Throughout my self-learning journey, I have
+                  developed a strong work ethic, attention to detail, and the
+                  ability to work independently to achieve my goals.
+                </p>
+                <a
+                  className="download"
+                  href="../../assets/img/1-23 CV.pdf"
+                  download
+                >
+                  Download CV
+                </a>
+              </div>
+            </div>
           </div>
         </header>
       </div>
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,8 +1,16 @@
 import { motion } from "framer-motion";
 import "./skills.css";
 
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+import htmlImg from "../../assets/img/html.png"
+import cssImg from "../../assets/img/css-3.png"
+import javascriptImg from "../../assets/img/js.png"
+import bootStrapImg from "../../assets/img/bootstrap.png"
+import sassImg from "../../assets/img/sass.png";
+import jqueryImg from "../../assets/img/jquery.png"
+import reactImg from "../../assets/img/react.png";
+import VueImg from "../../assets/img/Vue.png";
+import gitImg from "../../assets/img/github.png";
+import commandImg from "../../assets/img/command-line.png";
 
 export default function Skills() {
   return (
@@ -10,148 +18,183 @@ export default function Skills() {
       className="container pt-5"
       initial={{ width: 0 }}
       animate={{ width: "100%" }}
-      exit={{ x: window.innerWidth, transition:{duration: .3} }}
+      exit={{ x: window.innerWidth, transition: { duration: 0.3 } }}
     >
-      <h2 className="skills-title text-light mb-4 mt-5 ml-5 bill display-4">
+      <h2 className="skills-title text-center text-light mb-4 mt-5 ml-5 bill display-4">
         My Skills
       </h2>
       <div className="skills">
         <div className="row">
-          <div className="col-sm-4 col-lg-2 mb-3 ">
-            <CircularProgressbar
-              value={90}
-              text={`90%`}
-              styles={buildStyles({
-                // Rotation of path and trail, in number of turns (0-1)
-                rotation: 0,
-
-                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                strokeLinecap: "round",
-
-                // Text size
-                textSize: "16px",
-
-                // How long animation takes to go from one percentage to another, in seconds
-                pathTransitionDuration: 2,
-
-                // Can specify path transition in more detail, or remove it entirely
-                // pathTransition: 'none',
-
-                // Colors
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5">HTML5</h3>
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={htmlImg} alt="html" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">HTML5</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  HTML is the standard markup language for Web pages.
+                </p>
+              </div>
+            </div>
           </div>
 
-          <div className="col-sm-4 col-lg-2 mb-3 ">
-            <CircularProgressbar
-              value={85}
-              text={`85%`}
-              styles={buildStyles({
-                rotation: 0,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 2,
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5 ">CSS3</h3>
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={cssImg} alt="CSS" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">CSS</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  CSS is the language we use to style an HTML document. CSS
+                  describes how HTML elements should be displayed.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-4 col-lg-2 mb-3 ">
-            <CircularProgressbar
-              value={80}
-              text={`80%`}
-              styles={buildStyles({
-                rotation: 0,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 2,
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5 ">JavaScript</h3>
+
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img
+                  src={javascriptImg}
+                  alt="JavaScript"
+                  className="img-fluid w-25"
+                />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">JavaScript</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  JavaScript is a scripting language that enables you to create
+                  dynamically updating content, control multimedia, animate
+                  images, and pretty much everything else.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-4 col-lg-2 mb-3 ">
-            <CircularProgressbar
-              value={80}
-              text={`80%`}
-              styles={buildStyles({
-                rotation: 0,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 2,
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5 ">Bootstrap</h3>
+
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img
+                  src={bootStrapImg}
+                  alt="Bootstrap"
+                  className="img-fluid w-25"
+                />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">Bootstrap</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  Bootstrap is the most popular HTML, CSS, and JavaScript
+                  framework for developing responsive, mobile-first websites.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-4 col-lg-2 mb-3 ">
-            <CircularProgressbar
-              value={70}
-              text={`70%`}
-              styles={buildStyles({
-                rotation: 0,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 2,
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5 ">jquery</h3>
+
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={sassImg} alt="Sass" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">Sass</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">Sass is a CSS pre-processor.</p>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-4 col-lg-2 mb-3">
-            <CircularProgressbar
-              value={50}
-              text={`50%`}
-              styles={buildStyles({
-                rotation: 0,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 2,
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5 ">
-              vue.js(Entry Level)
-            </h3>
+
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={jqueryImg} alt="Jquery" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">Jquery</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">jQuery is a JavaScript Library.</p>
+              </div>
+            </div>
           </div>
-          <div className="col-sm-4 col-lg-2 mb-3">
-            <CircularProgressbar
-              value={50}
-              text={`50%`}
-              styles={buildStyles({
-                rotation: 0,
-                strokeLinecap: "round",
-                textSize: "16px",
-                pathTransitionDuration: 2,
-                pathColor: `orange`,
-                textColor: "#fff",
-                trailColor: "#d6d6d6",
-                backgroundColor: "#3e98c7",
-              })}
-            />
-            <h3 className="text-center mt-3 text-light h5 ">
-              React.js(Entry Level)
-            </h3>
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={reactImg} alt="React" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">Reactjs</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  React is a JavaScript library for building user interfaces.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={VueImg} alt="Vuejs" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">Vuejs</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  Vue is a JavaScript library for building user interfaces.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img src={gitImg} alt="GitHub" className="img-fluid w-25" />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">GitHub</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  GitHub is a code hosting platform for version control and
+                  collaboration. It lets you and others work together on
+                  projects from anywhere.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-xm-12 col-sm-6 col-md-4 col-lg-3 mb-4 ">
+            <div className="skill-box p-4 rounded text-center">
+              <div className="skill-img text-center">
+                <img
+                  src={commandImg}
+                  alt="Command Line"
+                  className="img-fluid w-25"
+                />
+              </div>
+              <div className="skill-title">
+                <h3 className=" my-4 h2">Command Line</h3>
+              </div>
+              <div className="skill-desc">
+                <p className="lead ">
+                  The command line is a text interface for your computer. It’s a
+                  program that takes in commands, which it passes on to the
+                  computers operating system to run.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
